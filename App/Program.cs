e5193becfe2b12b87a9e0921d8d4400aa425f51c
Matvey <matvey.lipilin1@gmail.com>
2022-09-12 15:45:50 +0500

    Init commit

---
+++ App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// Configure Microsoft Dependency Injection
+var services = new ServiceCollection();
+
+// Create logger
+services.AddLogging(configure => configure.AddConsole());
+
+// Create service provider
+using var serviceProvider = services.BuildServiceProvider();
+
+// Test logger
+var logger = serviceProvider.GetService<ILogger<Program>>()!;
+logger.LogInformation("Hello World!");
+
