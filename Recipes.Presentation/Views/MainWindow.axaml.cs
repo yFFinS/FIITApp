db6aec5fb39e88cd9215e745291d2227ff2f8c1a
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2022-09-24 21:48:20 +0500

    search template wip

--- Recipes.Presentation/Views/MainWindow.axaml.cs
+++ Recipes.Presentation/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -5,11 +6,24 @@ namespace Recipes.Presentation.Views
 {
     public partial class MainWindow : Window
     {
+        private Panel MainPanel;
         public MainWindow()
         {
             InitializeComponent();
+            MainPanel = this.FindControl<Panel>("Main")!;
+        }
+
+        protected override void HandleWindowStateChanged(WindowState state)
+        {
+            base.HandleWindowStateChanged(state);
+            MainPanel.Margin = WindowState == WindowState.Maximized ? new Thickness(8) : new Thickness(0);
         }
 
         private void CloseBtn_OnClick(object? sender, RoutedEventArgs e) => Close();
+
+        private void MaximizeBtn_OnClick(object? sender, RoutedEventArgs e) => WindowState =
+            WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        private void MinimizeBtn_OnClick(object? sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
     }
 }
---
+++ Recipes.Presentation/Views/SearchBar.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Recipes.Presentation.Views;
+
+public partial class SearchBar : UserControl
+{
+    public SearchBar()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
