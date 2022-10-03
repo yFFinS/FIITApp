692585ee3cd04e9db0e94f14a0c5570419387df6
Faddey Kryachko <95309709+faddeyca@users.noreply.github.com>
2022-10-03 02:21:10 -0700

    Avialable recipes by ingredients searcher add

---
+++ Recipes.Application/Interfaces/IRecipeFinderByIngredients.cs
@@ -0,0 +1,15 @@
+﻿using Recipes.Domain.Entities.RecipeAggregate;
+using Recipes.Domain.IngredientsAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Application.Interfaces;
+
+public interface IRecipeFinderByAvailableIngredients
+{
+    IEnumerable<Recipe> FindRecipesByAvailableIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients);
+}
+
---
+++ Recipes.Application/Services/RecipeFinderByAvailableIngredients.cs
@@ -0,0 +1,49 @@
+﻿using Recipes.Application.Interfaces;
+using Recipes.Domain.Entities.RecipeAggregate;
+using Recipes.Domain.IngredientsAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Application.Services;
+
+public class RecipeFinderByAvailableIngredients : IRecipeFinderByAvailableIngredients
+{
+    public IEnumerable<Recipe> FindRecipesByAvailableIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients)
+    {
+        var resultRecipes = new List<Recipe>();
+        var ingredientsCollection = ingredients.AsReadOnlyCollection();
+        foreach (var recipe in recipes)
+        {
+            var recipeContainsAll = true;
+            foreach (var ingredient in ingredientsCollection)
+            {
+                var product = ingredient.Product.Id;
+                var productQuantity = ingredient.Quantity;
+                var contains = false;
+                foreach (var recipeIngredient in recipe.Ingredients)
+                {
+                    var recipeProduct = recipeIngredient.Product.Id;
+                    var recipeProductQuantity = recipeIngredient.Quantity;
+                    if (recipeProduct == product && productQuantity >= recipeProductQuantity)
+                    {
+                        contains = true;
+                        break;
+                    }
+                }
+                if (!contains)
+                {
+                    recipeContainsAll = false;
+                    break;
+                }
+            }
+            if (recipeContainsAll)
+            {
+                resultRecipes.Add(recipe);
+            }
+        }
+        return resultRecipes;
+    }
+}
