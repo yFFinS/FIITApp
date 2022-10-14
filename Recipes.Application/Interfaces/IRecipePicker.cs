7e2dc2ccedac06a6b7d5d7d631edd01e64a20e24
Faddey Kryachko <95309709+faddeyca@users.noreply.github.com>
2022-10-14 23:18:56 +0500

    refactoring

--- Recipes.Application/Interfaces/IRecipePicker.cs
+++ Recipes.Application/Interfaces/IRecipePicker.cs
@@ -1,17 +1,21 @@
 ﻿using Recipes.Domain.Entities.RecipeAggregate;
 using Recipes.Domain.IngredientsAggregate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Recipes.Application.Interfaces;
 
 public interface IRecipePicker
 {
-    IEnumerable<Recipe> PickRecipesByIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients);
+    IEnumerable<Recipe> PickRecipesByIngredients(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients);
 
-    IEnumerable<Recipe> PickRecipesByAvailableIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients);
+    IEnumerable<Recipe> PickRecipesByAvailableIngredients(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients);
+
+    IEnumerable<Recipe> PickRecipesByAvailableIngredientsWithMarginOfError(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients,
+        double margin);
 }
 
--- Recipes.Application/Services/RecipePicker.cs
+++ Recipes.Application/Services/RecipePicker.cs
@@ -1,72 +1,46 @@
-﻿using Recipes.Application.Interfaces;
+﻿using Ardalis.GuardClauses;
+using Recipes.Application.Interfaces;
 using Recipes.Domain.Entities.RecipeAggregate;
 using Recipes.Domain.IngredientsAggregate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Recipes.Application.Services;
 
 public class RecipePicker : IRecipePicker
 {
-    public IEnumerable<Recipe> PickRecipesByAvailableIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients)
+    public IEnumerable<Recipe> PickRecipesByIngredients(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients)
     {
-        return PickRecipesByParameters(recipes, ingredients, ByAvailableIngredients);
+        var ingredientsDictionary = ingredients.ToDictionary(ingredient => ingredient.ProductId);
+        return recipes
+            .Where(recipe =>
+                !recipe.Ingredients.Any(ingredient =>
+                    !ingredientsDictionary.ContainsKey(ingredient.ProductId)));
     }
 
-    public IEnumerable<Recipe> PickRecipesByIngredients(IEnumerable<Recipe> recipes, IngredientGroup ingredients)
+    public IEnumerable<Recipe> PickRecipesByAvailableIngredients(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients)
     {
-        return PickRecipesByParameters(recipes, ingredients, ByIngredients);
+        var ingredientsDictionary = ingredients.ToDictionary(ingredient => ingredient.ProductId);
+        return recipes
+            .Where(recipe => 
+                !recipe.Ingredients.Any(ingredient =>
+                    !ingredientsDictionary.ContainsKey(ingredient.ProductId) ||
+                    ingredientsDictionary[ingredient.ProductId].Quantity < ingredient.Quantity));
     }
 
-    private bool ByAvailableIngredients(Ingredient ingredient, Ingredient recipeIngredient)
+    public IEnumerable<Recipe> PickRecipesByAvailableIngredientsWithMarginOfError(
+        IEnumerable<Recipe> recipes,
+        IngredientGroup ingredients,
+        double margin)
     {
-        var product = ingredient.Product.Id;
-        var productQuantity = ingredient.Quantity;
-        var recipeProduct = recipeIngredient.Product.Id;
-        var recipeProductQuantity = recipeIngredient.Quantity;
-        return recipeProduct == product && productQuantity >= recipeProductQuantity;
-    }
-
-    private bool ByIngredients(Ingredient ingredient, Ingredient recipeIngredient)
-    {
-        var product = ingredient.Product.Id;
-        var recipeProduct = recipeIngredient.Product.Id;
-        return recipeProduct == product;
-    }
-
-    private IEnumerable<Recipe> PickRecipesByParameters(IEnumerable<Recipe> recipes, IngredientGroup ingredients,
-        Func<Ingredient, Ingredient, bool> comparator)
-    {
-        var resultRecipes = new List<Recipe>();
-        var ingredientsCollection = ingredients.AsReadOnlyCollection();
-        foreach (var recipe in recipes)
-        {
-            var recipeContainsAll = true;
-            foreach (var ingredient in ingredientsCollection)
-            {
-                var contains = false;
-                foreach (var recipeIngredient in recipe.Ingredients)
-                {
-                    if (comparator(ingredient, recipeIngredient))
-                    {
-                        contains = true;
-                        break;
-                    }
-                }
-                if (!contains)
-                {
-                    recipeContainsAll = false;
-                    break;
-                }
-            }
-            if (recipeContainsAll)
-            {
-                resultRecipes.Add(recipe);
-            }
-        }
-        return resultRecipes;
+        margin = Guard.Against.NegativeOrZero(margin);
+        var ingredientsDictionary = ingredients.ToDictionary(ingredient => ingredient.ProductId);
+        return recipes
+            .Where(recipe =>
+                !recipe.Ingredients.Any(ingredient =>
+                    !ingredientsDictionary.ContainsKey(ingredient.ProductId) ||
+                    ingredientsDictionary[ingredient.ProductId].Quantity < ingredient.Quantity));
     }
 }
