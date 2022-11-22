8ebb963f84f3f08919c1ef678c6170b05f70d454
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2022-11-22 16:17:35 +0500

    of type done

---
+++ Recipes.Presentation/Controls/SearchBar.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Recipes.Presentation.Controls;
+
+public partial class SearchBar : UserControl
+{
+    public SearchBar()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
--- Recipes.Presentation/ViewModels/SelectionViewModel.cs
+++ Recipes.Presentation/ViewModels/SelectionViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
+using Avalonia.Controls;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Logging.Abstractions;
 using ReactiveUI;
 using Recipes.Application.Services.Preferences;
 using Recipes.Application.Services.RecipePicker;
+using Recipes.Domain.Entities.ProductAggregate;
 using Recipes.Presentation.Interfaces;
 
 namespace Recipes.Presentation.ViewModels;
@@ -13,6 +17,8 @@ public class SelectionViewModel : ViewModelBase
 {
     private readonly SearchViewModel _standardSearch;
     
+    public ObservableCollection<Product> Products { get; private set; }
+
     public SelectionViewModel(Lazy<IViewContainer> container, SearchViewModel standardSearch)
     {
         _standardSearch = standardSearch;
@@ -20,6 +26,8 @@ public class SelectionViewModel : ViewModelBase
         {
             container.Value.Content = _standardSearch;
         });
+
+        Products = new ObservableCollection<Product>(Enumerable.Empty<Product>());
     }
 
     public ReactiveCommand<Unit, Unit> ShowStandardSearch { get; }
