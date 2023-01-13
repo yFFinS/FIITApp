8aed50b0bda1ed90628cbe9e47875519c749fef8
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2023-01-13 12:57:20 +0500

    fix: recipe layout

---
+++ Recipes.Presentation/Controls/RecipeAdditionalInfoLabel.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Recipes.Presentation.Controls;
+
+public partial class RecipeAdditionalInfoLabel : UserControl
+{
+    public RecipeAdditionalInfoLabel()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
